442047e049fd3d2627681ed085ab48ac15f11afa
Thomas Meyer <strayfatty@gmail.com>
2019-06-09 21:13:35 +0200

    Canvas to PPM

---
+++ src/Tray.Core.Tests/CanvasExtensionTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Tray.Core.Tests
+{
+    public sealed class CanvasExtensionTests
+    {
+        [Fact]
+        public void ToPPM_Header()
+        {
+            var canvas = new Canvas(5, 3);
+
+            var actual = canvas.ToPPM();
+
+            actual.Should().StartWith("P3\n5 3\n255\n");
+        }
+
+        [Fact]
+        public void ToPPM_PixelData()
+        {
+            var canvas = new Canvas(5, 3);
+            canvas[0, 0] = new Color(1.5f, 0.0f, 0.0f);
+            canvas[2, 1] = new Color(0.0f, 0.5f, 0.0f);
+            canvas[4, 2] = new Color(-0.5f, 0.0f, 1.0f);
+
+            var actual = canvas.ToPPM();
+
+            var line4 = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n";
+            var line5 = "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n";
+            var line6 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n";
+            actual.Should().EndWith(line4 + line5 + line6);
+        }
+
+        [Fact]
+        public void ToPPM_SplittingLongLines()
+        {
+            var canvas = new Canvas(10, 2);
+            var color = new Color(1.0f, 0.8f, 0.6f);
+
+            for (var x = 0; x < 10; ++x)
+                for (var y = 0; y < 2; ++y)
+                    canvas[x, y] = color;
+
+            var actual = canvas.ToPPM();
+
+            var line4 = "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n";
+            var line5 = "153 255 204 153 255 204 153 255 204 153 255 204 153\n";
+            var line6 = "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204\n";
+            var line7 = "153 255 204 153 255 204 153 255 204 153 255 204 153\n";
+            actual.Should().EndWith(line4 + line5 + line6 + line7);
+        }
+
+        [Fact]
+        public void ToPPM_TerminatedByANewline()
+        {
+            var canvas = new Canvas(5, 3);
+
+            var actual = canvas.ToPPM();
+
+            actual.Should().EndWith("\n");
+        }
+    }
+}
---
+++ src/Tray.Core/CanvasExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace Tray.Core
+{
+    public static class CanvasExtensions
+    {
+        public static string ToPPM(this ICanvas canvas)
+        {
+            var s = new StringBuilder();
+            s.Append("P3\n");
+            s.Append($"{canvas.Width} {canvas.Height}\n");
+            s.Append("255");
+
+            int remainder = 0;
+            for (var y = 0; y < canvas.Height; ++y)
+            {
+                for (var x = 0; x < canvas.Width; ++x)
+                {
+                    var color = canvas[x, y];
+                    var red = ColorValue(color.R);
+                    var green = ColorValue(color.G);
+                    var blue = ColorValue(color.B);
+
+                    remainder = Append(red);
+                    remainder = Append(green);
+                    remainder = Append(blue);
+                }
+
+                if (s[s.Length - 1] != '\n')
+                {
+                    s.Append("\n");
+                    remainder = 70;
+                }
+            }
+
+            if (s[s.Length - 1] != '\n')
+                s.Append("\n");
+
+            return s.ToString();
+
+            string ColorValue(float value)
+            {
+                var intValue = (int)MathF.Round(value * 255, MidpointRounding.AwayFromZero);
+                var clamped = MathF.Max(0, MathF.Min(intValue, 255));
+                return $"{clamped}";
+            }
+
+            int Append(string value)
+            {
+                var separator = s[s.Length - 1] != '\n' ? " " : string.Empty;
+                var newRemainder = remainder - (1 + value.Length);
+                if (newRemainder < 1)
+                {
+                    separator = "\n";
+                    newRemainder = 70 - value.Length;
+                }
+
+                s.Append(separator);
+                s.Append(value);
+                return newRemainder;
+
+            }
+        }
+    }
+}
