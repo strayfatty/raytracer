e592c6a873063ca6bd58916980a7d6c5abc498ed
Thomas Meyer <strayfatty@gmail.com>
2019-06-22 11:08:48 +0200

    Matrix4x4 AboutEquals

---
+++ src/Tray.Core.Tests/Matrix4x4ExtensionTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Tray.Core.Tests
+{
+    public sealed class Matrix4x4ExtensionTests
+    {
+        [Fact]
+        public void AboutEquals_IdentityIdentity_True()
+        {
+            var a = Matrix4x4.Identity;
+            var b = Matrix4x4.Identity;
+
+            var actual = a.AboutEquals(b);
+
+            actual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void AboutEquals_IdentityTranslation_False()
+        {
+            var a = Matrix4x4.Identity;
+            var b = Matrix4x4.CreateTranslation(0.0f, 0.0f, 1.0f);
+
+            var actual = a.AboutEquals(b);
+
+            actual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void AboutEquals_Inverse_True()
+        {
+            var a = new Matrix4x4(
+                 8.0f, -5.0f,  9.0f,  2.0f,
+                 7.0f,  5.0f,  6.0f,  1.0f,
+                -6.0f,  0.0f,  9.0f,  6.0f,
+                -3.0f,  0.0f, -9.0f, -4.0f);
+            var b = new Matrix4x4(
+                -0.15385f, -0.15385f, -0.28205f, -0.53846f,
+                -0.07692f,  0.12308f,  0.02564f,  0.03077f,
+                 0.35897f,  0.35897f,  0.43590f,  0.92308f,
+                -0.69231f, -0.69231f, -0.76923f, -1.92308f);
+
+            Matrix4x4 inverted;
+            Matrix4x4.Invert(a, out inverted);
+            var actual = inverted.AboutEquals(b);
+
+            actual.Should().BeTrue();
+        }
+    }
+}
---
+++ src/Tray.Core/Matrix4x4Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace Tray.Core
+{
+    public static class Matrix4x4Extensions
+    {
+        public static bool AboutEquals(this Matrix4x4 a, Matrix4x4 b)
+        {
+            return a.M11.AboutEquals(b.M11)
+                && a.M12.AboutEquals(b.M12)
+                && a.M13.AboutEquals(b.M13)
+                && a.M14.AboutEquals(b.M14)
+                && a.M21.AboutEquals(b.M21)
+                && a.M22.AboutEquals(b.M22)
+                && a.M23.AboutEquals(b.M23)
+                && a.M24.AboutEquals(b.M24)
+                && a.M31.AboutEquals(b.M31)
+                && a.M32.AboutEquals(b.M32)
+                && a.M33.AboutEquals(b.M33)
+                && a.M34.AboutEquals(b.M34)
+                && a.M41.AboutEquals(b.M41)
+                && a.M42.AboutEquals(b.M42)
+                && a.M43.AboutEquals(b.M43)
+                && a.M44.AboutEquals(b.M44);
+        }
+    }
+}
