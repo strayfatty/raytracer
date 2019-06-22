f580c98000d7e3956b293690f1a91cb6d821b54c
Thomas Meyer <strayfatty@gmail.com>
2019-06-22 11:39:22 +0200

    Shearing matrix

---
+++ src/Tray.Core.Tests/Matrix4x4FactoryTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Tray.Core.Tests
+{
+    public sealed class Matrix4x4FactoryTests
+    {
+        [Fact]
+        public void CreateShearing_XY()
+        {
+            var m = Matrix4x4Factory.CreateShearing(1, 0, 0, 0, 0, 0);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(5, 3, 4, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateShearing_XZ()
+        {
+            var m = Matrix4x4Factory.CreateShearing(0, 1, 0, 0, 0, 0);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(6, 3, 4, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateShearing_YX()
+        {
+            var m = Matrix4x4Factory.CreateShearing(0, 0, 1, 0, 0, 0);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(2, 5, 4, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateShearing_YZ()
+        {
+            var m = Matrix4x4Factory.CreateShearing(0, 0, 0, 1, 0, 0);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(2, 7, 4, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateShearing_ZX()
+        {
+            var m = Matrix4x4Factory.CreateShearing(0, 0, 0, 0, 1, 0);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(2, 3, 6, 1)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CreateShearing_ZY()
+        {
+            var m = Matrix4x4Factory.CreateShearing(0, 0, 0, 0, 0, 1);
+            var p = new Vector4(2, 3, 4, 1);
+
+            var actual = Vector4.Transform(p, m);
+
+            actual.AboutEquals(new Vector4(2, 3, 7, 1)).Should().BeTrue();
+        }
+    }
+}
---
+++ src/Tray.Core/Matrix4x4Factory.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Tray.Core
+{
+    public static class Matrix4x4Factory
+    {
+        public static Matrix4x4 CreateShearing(
+            float xy,
+            float xz,
+            float yx,
+            float yz,
+            float zx,
+            float zy)
+        {
+            return new Matrix4x4(
+                1, yx, zx, 0,
+                xy, 1, zy, 0,
+                xz, yz, 1, 0,
+                0, 0, 0, 1);
+        }
+    }
+}
